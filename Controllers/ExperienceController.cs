c99c41882eb04e208667e66ddf27d210ac3d9d50
Musa <mbhc4235@gmail.com>
2024-09-19 23:04:51 +0300

    Something gonna happen

--- Controllers/ExperienceController.cs
+++ Controllers/ExperienceController.cs
@@ -1,12 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolioWebsite.DAL.Context;
 
 namespace MyPortfolioWebsite.Controllers
 {
     public class ExperienceController : Controller
     {
+        PortfolioContext context = new PortfolioContext();
         public IActionResult ExperinceList()
         {
-            return View();
+            var values = context.Experiences.ToList();
+            return View(values);
         }
     }
 }
