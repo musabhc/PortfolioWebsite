3c24cc5b0d2b1a9d8ce0ba3a6ffb636685b0bd02
Musa <mbhc4235@gmail.com>
2024-09-24 12:41:26 +0300

    Contact and About Section complated

--- Controllers/AboutController.cs
+++ Controllers/AboutController.cs
@@ -12,25 +12,6 @@ namespace MyPortfolioWebsite.Controllers
             var values = context.Abouts.ToList();
             return View(values);
         }
-        [HttpGet]
-        public IActionResult CreateAbout()
-        {
-            return View();
-        }
-        [HttpPost]
-        public IActionResult CreateAbout(About about) 
-        { 
-            context.Abouts.Add(about);
-            context.SaveChanges();
-			return RedirectToAction("AboutList");
-		}
-		public IActionResult DeleteAbout(int id)
-		{
-			var value = context.Abouts.Find(id);
-			context.Abouts.Remove(value);
-			context.SaveChanges();
-			return RedirectToAction("AboutList");
-		}
 		[HttpGet]
 		public IActionResult UpdateAbout(int id)
 		{
